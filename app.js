8787434b7d2db4733d6008faf24ff63227e3b861
and-444 <andreysavinx@yandex.ru>
2024-04-27 02:05:29 +0300

    finish part-1 project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+const connectToDatabase = require("./database/connect");
+const app = express();
+
+connectToDatabase();
+const PORT = 3000;
+app.use(
+  bodyParser.json(),
+  express.static(path.join(__dirname, "public")),
+  usersRouter,
+  gamesRouter,
+  categoriesRouter
+);
+
+app.listen(PORT);
