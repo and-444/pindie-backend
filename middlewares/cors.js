387e5e8048d5b0d8ae21351f6432367ab653db0f
and-444 <andreysavinx@yandex.ru>
2024-05-16 20:51:46 +0300

    fix file cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,15 +1,21 @@
 const allowedCors = [
   "https://practicum.yandex.ru",
   "https://students-projects.ru",
+  "https://and-pindie-frontend.nomoredomainswork.ru",
   "http://localhost:3000",
   "http://localhost:3001",
-  "http://localhost:3001/api/users/me",
 ];
 function cors(req, res, next) {
   const { origin } = req.headers;
+
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
   next();
 }
 
