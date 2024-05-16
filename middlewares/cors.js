de6df3ea0fb6421e900d3eecb370adfc0e04621b
and-444 <andreysavinx@yandex.ru>
2024-05-17 01:07:26 +0300

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,14 +3,29 @@ const allowedCors = ["https://and-pindie-frontend.nomoredomainswork.ru"];
 function cors(req, res, next) {
   const { origin } = req.headers;
 
+  //   if (allowedCors.includes(origin)) {
+  //     res.header("Access-Control-Allow-Origin", origin);
+  //   }
+  //   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  //   );
+  //   next();
+  // }
+
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
   }
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-  );
+
   next();
 }
 
