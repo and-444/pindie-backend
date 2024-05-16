97968eeb3c727b8ea5ade79550b1284658b683fd
and-444 <andreysavinx@yandex.ru>
2024-05-16 23:22:27 +0300

    cors fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   "https://practicum.yandex.ru",
   "https://students-projects.ru",
-  "https://and-pindie-frontend.nomoredomainswork.ru/",
+  "https://and-pindie-frontend.nomoredomainswork.ru",
   "http://localhost:3000",
   "http://localhost:3001",
 ];
@@ -14,7 +14,6 @@ function cors(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Origin",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
   );
   next();
